8fa2f3037fbff205dabd873fdceb6e669a8f2ce4
Jens Zalzala <jens@shakingearthdigital.com>
2018-07-28 16:39:38 -0700

    added trails to planets

--- app/Assets/Scripts/PlanetThrow.cs
+++ app/Assets/Scripts/PlanetThrow.cs
@@ -65,6 +65,8 @@ public class PlanetThrow : MonoBehaviour {
 			rigidbody.useGravity = false;
 			rigidbody.mass = go.GetComponent<Planet>().mass;
 
+			go.GetComponent<TrailRenderer>().enabled = true;
+
 			go.AddComponent<OrbitalMotion>();
 		}
 	}
