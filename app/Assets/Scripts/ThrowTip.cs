7c5374efd3e9749e76d577044e0f59831971bda9
Jens Zalzala <jens@shakingearthdigital.com>
2018-07-28 15:47:03 -0700

    code cleanup

--- app/Assets/Scripts/ThrowTip.cs
+++ app/Assets/Scripts/ThrowTip.cs
@@ -7,7 +7,6 @@ public class ThrowTip : MonoBehaviour {
 	public Transform Controller;
 	// Use this for initialization
 	void Start () {
-		print("ThrowTip : Start");
 		transform.SetParent(Controller);
 		transform.localPosition = new Vector3(0,0,0);
 	}
