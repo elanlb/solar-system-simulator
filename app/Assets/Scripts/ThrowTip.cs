ae97727123946f8faf65eff0e9b59d71ded5ef6c
Jens Zalzala <jens@shakingearthdigital.com>
2018-07-28 14:33:43 -0700

    added scripts & prefabs for throwing

---
+++ app/Assets/Scripts/ThrowTip.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowTip : MonoBehaviour {
+
+	public Transform ControllerTip;
+	// Use this for initialization
+	void Start () {
+		print("ThrowTip : Start");
+		transform.SetParent(ControllerTip);
+		transform.localPosition = new Vector3(0,0,0);
+	}
+}
