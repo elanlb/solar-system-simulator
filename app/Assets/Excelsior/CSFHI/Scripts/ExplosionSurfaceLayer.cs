92f43dd94ebb9e5dd787083bf854ee220f392b08
Jens Zalzala <jens@shakingearthdigital.com>
2018-07-29 09:28:58 -0700

    explosions working

---
+++ app/Assets/Excelsior/CSFHI/Scripts/ExplosionSurfaceLayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionSurfaceLayer : MonoBehaviour {
+	private float life = 0;
+
+	void Update () {
+		// fade out over time
+		var material = GetComponent<Renderer>().material;
+
+		material.SetColor("_EmissionColor", Color.Lerp(Color.white * 2, Color.black,life));
+		
+		life += Time.deltaTime * 2 ;
+
+		transform.localScale *= 1-Time.deltaTime;
+
+		// destroy if old enough
+		if (life > .9){
+			GameObject.Destroy(gameObject);
+		}
+	}
+}
--- app/Assets/Scripts/OrbitalMotion.cs
+++ app/Assets/Scripts/OrbitalMotion.cs
@@ -7,7 +7,9 @@ public class OrbitalMotion : MonoBehaviour {
 	private Vector3 goal = Vector3.zero;
 	private Rigidbody rb;
 	private Vector3 force;
-	// Use this for initialization
+	
+	private bool exploding = false;
+
 	void Start () {
         rb = GetComponent<Rigidbody>();
 	}
@@ -24,19 +26,43 @@ public class OrbitalMotion : MonoBehaviour {
 	}
 
 	void OnCollisionEnter(Collision collision){
-		print(collision.collider.gameObject.name);
+		if (exploding) return;
+
 		if (collision.collider.gameObject.name.Equals("Sun")){
 			// sun collision
-			// explode
-			GameObject.Destroy(gameObject, .5f);
-
-			var explosion = GameObject.Instantiate(GameObject.Find("Explosion"));
-
-			explosion.transform.position = transform.position;
-			for (int i = 0; i < transform.childCount; i++){
-				explosion.transform.GetChild(i).gameObject.SetActive(true);
-			}
-
+			exploding = true;
+			StartCoroutine(ExplosionRoutine());
 		}
+	}
+
+	IEnumerator ExplosionRoutine(){
+		var explosion = GameObject.Instantiate(GameObject.Find("Explosion"));
+
+		explosion.transform.position = transform.position;
+
+		// enable surface layer
+		var surfaceLayer = explosion.transform.GetChild(1);
+		var dia = GetComponent<SphereCollider>().radius *2;
+		surfaceLayer.transform.localScale = new Vector3(dia, dia, dia);
+		surfaceLayer.gameObject.SetActive(true);
+
+		// enable particle system
+		explosion.transform.GetChild(0).gameObject.SetActive(true);
+		var flare = explosion.transform.GetChild(2);
+		flare.transform.rotation = Random.rotation;
+		flare.gameObject.SetActive(true);
+		
+		float explosiontime = 0;
+		float planetScale = .9f;
+
+		while (planetScale > .5f){
+			explosiontime += Time.deltaTime;
+			planetScale = planetScale-(explosiontime*explosiontime);
+
+			transform.localScale = new Vector3(planetScale, planetScale, planetScale);
+			yield return null;
+		}
+
+		GameObject.Destroy(gameObject);
 	}
 }
